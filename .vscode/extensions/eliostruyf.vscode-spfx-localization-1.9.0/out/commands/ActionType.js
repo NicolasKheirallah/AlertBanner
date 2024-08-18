cd96f8806633446d8c6f6c57fbfb48c80becd69f
Nicolas Kheirallah <Nicolas.kheirallah@Exobe.com>
2024-08-18 08:19:02 +0200

    initial release

--- .vscode/extensions/eliostruyf.vscode-spfx-localization-1.9.0/out/commands/ActionType.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ActionType = void 0;
-var ActionType;
-(function (ActionType) {
-    ActionType[ActionType["create"] = 1] = "create";
-    ActionType[ActionType["insert"] = 2] = "insert";
-})(ActionType = exports.ActionType || (exports.ActionType = {}));
-//# sourceMappingURL=ActionType.js.map
--- .vscode/extensions/eliostruyf.vscode-spfx-localization-1.9.0/out/constants/CsvHeaders.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TIMESTAMP_HEADER = exports.COMMENT_HEADER = exports.LOCALE_HEADER = exports.KEY_HEADER = void 0;
-exports.KEY_HEADER = "key";
-exports.LOCALE_HEADER = "Locale";
-exports.COMMENT_HEADER = "Comment";
-exports.TIMESTAMP_HEADER = "Timestamp";
-//# sourceMappingURL=CsvHeaders.js.map
